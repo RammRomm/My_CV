f8792b2af49fcbb0c0e2aed4e26ac1ef9309285e
RammRomm <113111550+RammRomm@users.noreply.github.com>
2023-06-29 13:38:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,104 @@ fetch('https://weatherbit-v1-mashape.p.rapidapi.com/forecast/minutely?lat=35.5&l
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
+
+
+const timeLine = gsap.timeline({ default: { duration: 1 } });
+
+timeLine
+  .to(".banner", { opacity: 1 }, 1)
+  .to(".mine", {
+    scrollTrigger: {
+      trigger: ".mine",
+      scrub: true,
+      toggleActions: "restart pause reverse pause",
+    },
+    rotation: 5,
+    rotation: -5,
+    duration: 5,
+  })
+  .from(".steps", {
+    scrollTrigger: {
+      trigger: ".steps",
+      scrub: true,
+      toggleActions: "restart pause reverse pause",
+    },
+    rotation: 10,
+    rotation: -10,
+    duration: 2,
+    ease: "elastic",
+  })
+  .to(".ser", {
+    scrollTrigger: {
+      trigger: ".ser",
+    },
+    opacity: 1,
+    ease: "elastic",
+  })
+  .fromTo(
+    ".servicesBx",
+
+    {
+      scrollTrigger: {
+        trigger: ".servicesBx",
+        scrub: true,
+        toggleActions: "restart pause reverse pause",
+      },
+      opacity: 0,
+      x: "-1000%",
+      stagger: 0.5,
+    },
+    {
+      scrollTrigger: {
+        trigger: ".servicesBx",
+        scrub: true,
+        toggleActions: "restart pause reverse pause",
+      },
+      x: 0,
+      opacity: 1,
+      stagger: 0.5,
+    }
+  )
+  .fromTo(
+    ".workBx",
+    {
+      scrollTrigger: {
+        trigger: ".workBx",
+        scrub: true,
+        toggleActions: "restart pause reverse pause",
+      },
+      opacity: 0,
+      x: "-1000%",
+      stagger: 0.5,
+    },
+    {
+      scrollTrigger: {
+        trigger: ".workBx",
+        scrub: true,
+        toggleActions: "restart pause reverse pause",
+      },
+      x: 0,
+      opacity: 1,
+      stagger: 0.5,
+    }
+  )
+
+  .fromTo(
+    ".testimonialBx",
+    {
+      scrollTrigger: {
+        trigger: ".testimonialBx",
+        scrub: true,
+        toggleActions: "restart pause reverse pause",
+      },
+      scale: 1,
+    },
+    {
+      scrollTrigger: {
+        trigger: ".testimonialBx",
+        scrub: true,
+        toggleActions: "restart pause reverse pause",
+      },
+      scale: 0.75,
+    }
+  );
